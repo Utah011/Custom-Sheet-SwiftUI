7a2e384bc68b1bc0f1d66186e0f7e29647fd3d0f
Андрей Колбасов <kolbasovofficial@gmail.com>
2022-02-18 15:14:25 +0300

    Initial Commit

---
+++ Shared/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Shared
+//
+//  Created by GalahadPersival on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
